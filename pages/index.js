e574ce351247c47b2b1ec03c348b6622e36d8b58
Camila Sales <salescamila1@gmail.com>
2021-01-25 00:22:40 -0400

    Update with my name

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
       <main>
         <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js! Site</a>
+          <p>Camila Sales</p>
         </h1>
 
         <p className="description">
